74d0f1e361cca8f3785d65957b89fb399258b39b
Mawin Chuangkud <weena.chuangkud@gmail.com>
2025-12-31 18:43:34 +0700

    Update Configs.lua

--- src/FastCast2/Configs.lua
+++ src/FastCast2/Configs.lua
@@ -1,8 +1,18 @@
+--[[
+	- Author : Mawin CK 
+	- Date : 2025
+]]
 -- Haha, noob
 
 local Configs = {}
 
-Configs.DebugLogging = false
+Configs.DebugLogging = {
+	Casting = true,
+	Segment = true,
+	Hit = true,
+	RayPierce = true,
+	Calculation = true,
+}
 Configs.VisualizeCasts = false
 
 return Configs
